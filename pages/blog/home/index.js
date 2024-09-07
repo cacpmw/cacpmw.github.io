89376c673aa97035bcbc47b8269ad2327cc25ed6
Carlos Carneiro <carloscarneiropmw@gmail.com>
2024-09-07 16:57:04 -0300

    Closes issue#1 Added next, react and react-dom Added construction pages Added .nvmrc

---
+++ pages/blog/home/index.js
@@ -0,0 +1,8 @@
+function BlogHome(){
+
+    return (
+        <h1>Blog Home - Em construcao</h1>
+    )
+}
+
+export default BlogHome;
---
+++ pages/index.js
@@ -0,0 +1,8 @@
+function Landing(){
+
+    return(
+        <h1>Em construcao!</h1>
+    )
+}
+
+export default Landing;
