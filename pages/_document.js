a4e393d4b1da340fbf0ef64a1904b23a85f366d8
Carlos Carneiro <carloscarneiropmw@gmail.com>
2024-09-08 16:21:12 -0300

    adding _app.js and _document.js

---
+++ pages/_document.js
@@ -0,0 +1,13 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="pt-br">
+      <Head title="Carlos Carneiro" />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
