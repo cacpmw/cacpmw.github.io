f9a2399283e43e356104709cd2b0f569ab9ef2d1
Carlos <carloscarneiropmw@gmail.com>
2026-05-24 22:48:19 -0300

    Implement conversation event handling in plugin.js
    Added global variables and event listeners for conversation handling.

--- plugin.js
+++ plugin.js
@@ -8,3 +8,37 @@ async function callN8N(action, data) {
   });
   return response.json();
 }
+
+/********************
+ * Variáveis globais
+ ********************/
+var conversation
+var workspaceInfo
+
+/********************
+ * Eventos
+ ********************/
+
+Kinbox.on("conversation", function (data) {
+    conversation = data
+    console.log("on conversation", data)
+
+    getWorkspaceInfo()
+})
+
+Kinbox.on("no_conversation", function (data) {
+    conversation = null
+    console.log("on no-conversation", data)
+})
+
+Kinbox.on("callback", function (data) {
+    console.log("on callback", data)
+    if (data.key === "idade-changed") {
+        Kinbox.loading(false)
+        if (data.success) {
+            Kinbox.toast("success", "Alterou idade com sucesso")
+        } else {
+            Kinbox.toast("error", "Erro ao alterar idade")
+        }
+    }
+})
